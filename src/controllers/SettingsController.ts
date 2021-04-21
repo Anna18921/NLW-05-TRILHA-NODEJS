99d36c3ae88ea7ae279ec05788e611a7f7adbb17
Anna Rafaela Bispo de Santana <rafabispo53@gmail.com>
2021-04-21 15:58:04 -0300

    separate business rule controller for MessagesService

--- src/controllers/SettingsController.ts
+++ src/controllers/SettingsController.ts
@@ -1,22 +1,25 @@
 import { Request, Response } from "express";
-
-import { getCustomRepository } from "typeorm";
-import SettingsRepository from "../repositories/SettingsRepository";
+import SettingsService from "../services/SettingsService";
 
 export default class SettingsController {
 
     async create(request: Request, response: Response){
 
-        const {chat, username} = request.body;
+       try {
+            const {chat, username} = request.body;
+        
+            const settingsService = new SettingsService();
 
-        const settingsRepository = getCustomRepository(SettingsRepository);
-    
-        const settings = settingsRepository.create({chat, username});
-    
-        await settingsRepository.save(settings);
-    
-        return response.json({
-            settings
-        })
+            const settings = await settingsService.create({chat, username});
+
+            return response.json({
+                settings
+            });
+
+       } catch (error) {
+            return response.status(400).json({
+                message: error.message
+             });
+       }
     }
 }
---
+++ src/services/SettingsService.ts
@@ -0,0 +1,31 @@
+import { getCustomRepository, Repository } from "typeorm";
+import { Setting } from "../entities/Setting";
+import SettingsRepository from "../repositories/SettingsRepository";
+
+interface ISettingsCreate{
+    chat: boolean;
+    username: string;
+}
+
+export default class SettingsService {
+    private settingsRepository: Repository<Setting>;
+
+    constructor(){
+        this.settingsRepository = getCustomRepository(SettingsRepository);
+    }
+
+    async create({chat, username}: ISettingsCreate){
+       
+        const userAlreadyExist = await this.settingsRepository.findOne({username}) 
+
+        if (userAlreadyExist){
+            throw new Error("User already exists!");
+        }
+    
+        const settings = this.settingsRepository.create({chat, username});
+    
+        await this.settingsRepository.save(settings);
+
+        return settings;
+    }
+}
